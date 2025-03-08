ad4269e00e1716e53bab1e1800f070ef014b0b7d
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-03-08 19:20:22 +0530

    Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://newssence-assignment-backend.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
