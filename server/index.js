dcd48630bb87de7a003b0ee3813bd8157de885c2
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-03-08 18:46:53 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -15,11 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['https://news-digital.netlify.app/']
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
